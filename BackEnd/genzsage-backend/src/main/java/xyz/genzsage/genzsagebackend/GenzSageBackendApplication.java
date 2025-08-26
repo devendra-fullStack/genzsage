3c420762f628e15c6e94cc1ff04d519850d2a6db
devendra-fullStack <master.dev8857@gmail.com>
2025-08-26 22:34:07 +0530

    Initial commit

---
+++ BackEnd/genzsage-backend/src/main/java/xyz/genzsage/genzsagebackend/GenzSageBackendApplication.java
@@ -0,0 +1,13 @@
+package xyz.genzsage.genzsagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenzSageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenzSageBackendApplication.class, args);
+	}
+
+}
---
+++ BackEnd/genzsage-backend/src/test/java/xyz/genzsage/genzsagebackend/GenzSageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.genzsage.genzsagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenzSageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
